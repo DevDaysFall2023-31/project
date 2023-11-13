fb17b8fa660e6cd6aeb700772c89c381aa5ce4b8
san-kir-k <dfomka@list.ru>
2023-11-13 19:15:56 +0300

    Init version of audio cropping & supabase test

---
+++ app/src/main.py
@@ -0,0 +1,43 @@
+from pydub import AudioSegment
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+import os
+
+SECOND: int = 1_000
+AUDIO_DIR: str = "audio"
+CROPPED_DIR: str = "cropped"
+
+def crop_audio(filename: str) -> None:
+    full_filename: str = AUDIO_DIR + "/" + filename 
+    song = AudioSegment.from_mp3(full_filename)
+    song_mid: int = len(song) // 2
+    five_seconds: int = 5 * SECOND
+    first_10_seconds = song[song_mid - five_seconds: song_mid + five_seconds]
+    first_10_seconds.export(out_f='/'.join([AUDIO_DIR, CROPPED_DIR, filename]), format="mp3")
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    url = os.environ.get("SUPABASE_URL")
+    key = os.environ.get("SUPABASE_KEY")
+    
+    crop_audio("Mozart.mp3")
+
+    supabase: Client = create_client(url, key)
+    bucket_name: str = "test"
+
+    data = supabase.storage.from_(bucket_name).list()
+
+    for _, _, files in os.walk('/'.join([".", AUDIO_DIR, CROPPED_DIR])):
+        uploaded: list = list(map(lambda d: d.get('name', None), supabase.storage.from_(bucket_name).list()))
+        for file in files:
+            if file not in uploaded:
+                supabase.storage.from_(bucket_name).upload(
+                    file='/'.join([".", AUDIO_DIR, CROPPED_DIR, file]),
+                    path=file,
+                    file_options={"content-type": "audio/mpeg"}
+                )
+    
+    print(supabase.storage.from_(bucket_name).create_signed_url(path="Mozart.mp3", expires_in=60))
