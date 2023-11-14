2a0570deef62ae17cd3bb52cb692d69857d18c41
san-kir-k <dfomka@list.ru>
2023-11-14 19:39:06 +0300

    Add file clean up

--- test/parallel_runner.sh
+++ test/parallel_runner.sh
@@ -3,6 +3,10 @@
 PROCESSES_NUM=$1
 PY_SCRIPT_ARGS="${@:2}"
 
+if [ -f out.txt ]; then
+    > out.txt
+fi
+
 for i in $(seq 1 $PROCESSES_NUM); do
     python3 script.py $PY_SCRIPT_ARGS >> out.txt 2>&1 &
 done
