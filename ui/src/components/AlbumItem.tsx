105ed370fcc23347d8475081a49180e60faffe72
shishqa <shishqa.main@gmail.com>
2023-11-16 01:29:11 +0300

    feat(ui): play sound on hover

--- ui/src/components/AlbumItem.tsx
+++ ui/src/components/AlbumItem.tsx
@@ -1,12 +1,13 @@
 import React, { FC, useState } from "react";
 import '../styles/App.css';
-import music from "../assets/music.mp3";
 // @ts-ignore
 import useSound from 'use-sound';
 import { LinkCoverMusic } from "../assets/parser/LinkCoverMusic";
+import api from "../api";
+import supabase from "../supabase";
 
 export const AlbumItem: FC<any> = ({ albumInfo, create, count }) => {
-  // const [play, { stop }] = useSound(music);
+  const [audio, setAudio] = useState<HTMLAudioElement>();
 
   const addBlockMusic = () => {
     create(albumInfo);
@@ -21,15 +22,43 @@ export const AlbumItem: FC<any> = ({ albumInfo, create, count }) => {
     }
     return "large";
   }
+
+  const onMouseEnter = async () => {
+    if (audio) {
+      audio.play()
+    } else {
+      const response = await api.Backend.getPeakPeaksTrackIdGet(
+        albumInfo.id,
+        {
+          headers: {
+            Authorization: 'Bearer ' + (await supabase.auth.getSession()).data.session.access_token
+          }
+        }
+      ).catch((error) => {
+        console.error(error);
+      });
+      if (response) {
+        setAudio(new Audio(response.data.download_url));
+      }
+    }
+  }
+
+  const onMouseLeave = async () => {
+    if (audio) {
+      audio.pause()
+    }
+  }
+
   return (
     <div className={`album ${sizeName(count)}`}>
       <img
         onClick={addBlockMusic}
-        // onMouseEnter={() => play()}
-        // onMouseLeave={() => stop()}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
         src={LinkCoverMusic(albumInfo.cover_url)}
         alt={albumInfo.title}
       />
+      audio
     </div>
   )
 }
